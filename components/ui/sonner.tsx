f44218e96e20bc9dc28e23c764d1a09202182253
maybesabin <highsabin987@gmail.com>
2026-02-22 22:33:59 +0545

    ui: made login, signup page

---
+++ components/ui/sonner.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Toaster as Sonner, type ToasterProps } from 'sonner';
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  return (
+    <Sonner
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+          description: 'group-[.toaster]:text-muted-foreground',
+          actionButton:
+            'group-[.toaster]:bg-primary group-[.toaster]:text-primary-foreground',
+          cancelButton:
+            'group-[.toaster]:bg-muted group-[.toaster]:text-muted-foreground',
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
